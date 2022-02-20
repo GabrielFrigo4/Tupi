0d248c9dcdb01ea4ae2895fe9f0d32653cb37cf7
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2022-02-20 18:15:29 -0300

    Update code_ex.asm

--- Tupi/code_ex.asm
+++ Tupi/code_ex.asm
@@ -1,8 +1,8 @@
 ﻿.code
 main proc
-    sub rsp, 28h	;Reserve the shadow space
+	sub rsp, 28h	;Reserve the shadow space
 	mov rax, 5050
-    add rsp, 28h	;Remove shadow space
-    ret
+	add rsp, 28h	;Remove shadow space
+	ret
 main endp
 End
