e6070da1d2c21a80ed1113680ec27790cd2863a9
GabrielFrigo <gabriel.frigo4@gmail.com>
2022-03-26 21:23:08 -0300

    Update

--- TupiCompiler/TupiCompiler/Data/StructData.cs
+++ TupiCompiler/TupiCompiler/Data/StructData.cs
@@ -2,11 +2,20 @@
 internal class StructData
 {
     internal string Name { get; private set; }
+    internal int Size { get; set; }
     internal List<VarData> Vars { get; private set; }
 
     internal StructData(string name)
     {
         Name = name;
+        Size = 0;
+        Vars = new List<VarData>();
+    }
+
+    internal StructData(string name, int size)
+    {
+        Name = name;
+        Size = size;
         Vars = new List<VarData>();
     }
 }
