36bd1ab2836bd8f00778f489151a6af242984298
GabrielFrigo <gabriel.frigo4@gmail.com>
2022-05-19 21:52:17 -0300

    remove x86 libs

--- TupiLib/InternalTupiOperators/math.cpp
+++ TupiLib/InternalTupiOperators/math.cpp
@@ -3,10 +3,10 @@
 
 extern "C" double pow(double x, double y);
 
-double __internal__tupi__operate__math__pow(double x, double y) {
+extern "C" double __internal__tupi__operate__math__pow(double x, double y) {
 	return pow(x, y);
 }
 
-double __internal__tupi__operate__math__root(double x, double y) {
+extern "C" double __internal__tupi__operate__math__root(double x, double y) {
 	return pow(x, 1/y);
 }
--- TupiLib/InternalTupiOperators/memory.cpp
+++ TupiLib/InternalTupiOperators/memory.cpp
@@ -2,12 +2,12 @@
 #include "framework.h"
 #include "tupiWin.h"
 
-LPVOID __internal__tupi__operate__memory__new(size_t size) {
+extern "C" LPVOID __internal__tupi__operate__memory__new(size_t size) {
 	HANDLE hHeap = GetProcessHeap();
 	return HeapAlloc(hHeap, HEAP_NO_SERIALIZE, size);
 }
 
-BOOL __internal__tupi__operate__memory__delete(LPVOID lpMem) {
+extern "C" BOOL __internal__tupi__operate__memory__delete(LPVOID lpMem) {
 	HANDLE hHeap = GetProcessHeap();
 	return HeapFree(hHeap, HEAP_NO_SERIALIZE, lpMem);
 }
