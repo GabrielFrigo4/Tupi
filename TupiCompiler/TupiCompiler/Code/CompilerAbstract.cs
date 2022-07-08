50bb3bf11d5738721e33dd524110bbb4e57d4f61
GabrielFrigo <gabriel.frigo4@gmail.com>
2022-07-08 08:44:44 -0300

    Remove all warnings

--- TupiCompiler/TupiCompiler/Code/CompilerAbstract.cs
+++ TupiCompiler/TupiCompiler/Code/CompilerAbstract.cs
@@ -4,12 +4,81 @@ namespace TupiCompiler.Code;
 
 abstract internal class CompilerAbstract
 {
-    protected Func<string> CreateCode { get; set; }
+    private Func<string>? createCode = null;
+    private string? tupiCode = null;
+    private ReadOnlyData? readonlyData = null;
+    private RunData? runData = null;
+    private CompiledCode? compiledCode = null;
 
-    protected string TupiCode { get; set; }
-    protected ReadOnlyData ReadonlyData { get; set; }
-    protected RunData RunData { get; set; }
-    protected CompiledCode CompiledCode { get; set; }
+    protected Func<string> CreateCode {
+        get
+        {
+            if (createCode is not null)
+                return createCode;
+            else
+                throw new Exception("CreateCode not created");
+        }
+        set
+        {
+            createCode = value;
+        }
+    }
+    protected string TupiCode
+    {
+        get
+        {
+            if (tupiCode is not null)
+                return tupiCode;
+            else
+                throw new Exception("TupiCode not created");
+        }
+        set
+        {
+            tupiCode = value;
+        }
+    }
+    protected ReadOnlyData ReadonlyData
+    {
+        get
+        {
+            if (readonlyData is not null)
+                return readonlyData;
+            else
+                throw new Exception("ReadonlyData not created");
+        }
+        set
+        {
+            readonlyData = value;
+        }
+    }
+    protected RunData RunData
+    {
+        get
+        {
+            if (runData is not null)
+                return runData;
+            else
+                throw new Exception("RunData not created");
+        }
+        set
+        {
+            runData = value;
+        }
+    }
+    protected CompiledCode CompiledCode
+    {
+        get
+        {
+            if (compiledCode is not null)
+                return compiledCode;
+            else
+                throw new Exception("CompiledCode not created");
+        }
+        set
+        {
+            compiledCode = value;
+        }
+    }
 
     public event EventHandler<PreCompilerArgs>? PreCompilerEvent;
     public event EventHandler<CompilerArgs>? CompilerEvent;
